47f5d9c358f6821968e385afe6f379c880d5b243
KinSun <kinsun852@gmail.com>
2022-10-25 15:22:55 +0800

    Fix red background & remove team menu

--- src/components/ECNav.js
+++ src/components/ECNav.js
@@ -184,7 +184,7 @@ function ECNav(props) {
                                 <li><ECLink className="dropdown-item" url="https://ether.cards/videos" label="Video" /></li>
                                 <li><ECLink className="dropdown-item" url="https://blog.ether.cards/" label="Blog"/></li>
                                 <li><ECLink className="dropdown-item" url="https://docs.ether.cards/faq" label="FAQ"/></li>
-                                <li><ECLink className="dropdown-item" url="https://docs.ether.cards/team" label="Team"/></li>
+                                {/* <li><ECLink className="dropdown-item" url="https://docs.ether.cards/team" label="Team"/></li> */}
                             </ul>
                         </li>
                         <li className="nav-item">
